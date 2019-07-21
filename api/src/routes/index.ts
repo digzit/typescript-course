f1c6c3536194ed07f25b1c5f518eda5b93bacde9
Digzit <aurelien.digout@gmail.com>
2019-07-21 15:19:14 -0400

    create docker with mangoDB & mango-express

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -11,43 +11,5 @@ export class Routes {
             })
         })
 
-        // Contact
-        app.route('/contact')
-        // GET endpoint
-        .get((req: Request, res: Response) => {
-        // Get all contacts
-            res.status(200).send({
-                message: 'GET request successfulll!!!!'
-            })
-        })
-        // POST endpoint
-        .post((req: Request, res: Response) => {
-        // Create new contact
-            res.status(200).send({
-                message: 'POST request successfulll!!!!'
-            })
-        })
-
-        // Contact detail
-        app.route('/contact/:contactId')
-        // get specific contact
-        .get((req: Request, res: Response) => {
-        // Get a single contact detail
-            res.status(200).send({
-                message: 'GET request successfulll!!!!'
-            })
-        })
-        .put((req: Request, res: Response) => {
-        // Update a contact
-            res.status(200).send({
-                message: 'PUT request successfulll!!!!'
-            })
-        })
-        .delete((req: Request, res: Response) => {
-        // Delete a contact
-            res.status(200).send({
-                message: 'DELETE request successfulll!!!!'
-            })
-        })
     }
 }
