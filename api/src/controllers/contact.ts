b1e3652776b8f6493b08229103b1c296de970f41
Digzit <aurelien.digout@gmail.com>
2019-07-25 23:35:38 -0400

    type api

--- api/src/controllers/contact.ts
+++ api/src/controllers/contact.ts
@@ -4,21 +4,30 @@ import { Request, Response } from 'express';
 
 const Contact = mongoose.model('Contact', ContactSchema);
 
-export class ContactController{
+interface IContact {
+  _id?: string,
+  firstName: string,
+  lastName: string,
+  email: string,
+  company: string,
+  phone: number
+}
+
+export class ContactController {
 
   public add (req: Request, res: Response) {
     let newContact = new Contact(req.body);
-    newContact.save((err, contact) => {
+    newContact.save((err, contact: IContact) => {
       if(err){
         res.send(err);
       }
-      res.json(contact);
+      return res.json(contact);
     });
   }
 
 
   public getAll (req: Request, res: Response) {
-    Contact.find({}, (err, contact) => {
+    Contact.find({}, (err, contact: IContact[]) => {
       if(err){
         res.send(err);
       }
@@ -27,29 +36,29 @@ export class ContactController{
   }
 
   public getById (req: Request, res: Response) {
-    Contact.findById(req.params.contactId, (err, contact) => {
+    Contact.findById(req.params.contactId, (err, contact: IContact) => {
       if(err){
         res.send(err);
       }
-      res.json(contact);
+      return res.json(contact);
     });
   }
 
   public update (req: Request, res: Response) {
-    Contact.findOneAndUpdate({ _id: req.params.contactId }, req.body, { new: true }, (err, contact) => {
+    Contact.findOneAndUpdate({ _id: req.params.contactId }, req.body, { new: true }, (err, contact:IContact) => {
       if(err){
         res.send(err);
       }
-      res.json(contact);
+      return res.json(contact);
     });
   }
 
-  public delete (req: Request, res: Response) {
+  public delete (req: Request, res: Response)  {
     Contact.remove({ _id: req.params.contactId }, (err, contact) => {
       if(err){
         res.send(err);
       }
-      res.json({ message: 'Successfully deleted contact!'});
+      res.json({ succes: true });
     });
   }
 
